c61e8a055edd537bdc6100ea53abe810db3de53e
Delta.v <tmm.michalski@gmail.com>
2022-05-14 17:35:47 +0200

    add przyciskRzuc

--- 12_13_kosci/script.js
+++ 12_13_kosci/script.js
@@ -18,6 +18,7 @@ const nazwaGracz0 = document.querySelector('#nazwa-0');
 const nazwaGracz1 = document.querySelector('#nazwa-1');
 const panelGracz0 = document.querySelector('.gracz-0-panel');
 const panelGracz1 = document.querySelector('.gracz-1-panel');
+const przyciskRzuc = document.querySelector('.btn-rzuc');
 
 const obrazki = [
     'https://cdn.glitch.com/8fbc579f-3346-47a0-abbc-945a83abb962%2Fkosc-1.png?v=1610038358032',
@@ -52,3 +53,23 @@ function nowaGra() {
     panelGracz0.classList.add('aktywny');
     panelGracz1.classList.remove('aktywny');
 }
+nowaGra();
+
+przyciskRzuc.addEventListener('click', () => {
+    if (czyMoznaGrac) {
+        let liczbaOczek = Math.floor(Math.random() * 5) + 1;
+        kosc.src = obrazki[liczbaOczek - 1];
+        kosc.style.display = 'block';
+    }
+});
+
+/*
+1.Dodaj event listener od klikania do przycisku z klasą "btn-rzuc"
+2. Sprawdz czy zmienna czyMoznaGrac jest prawdziwa, jeżeli tak:
+    3. wylosuj do nowej zmiennej liczbę oczek za pomocą Math.random 1-6
+    4. pokaż w elemencie html o klacie "kosc" obrazek z tablicy o tej samej ilości oczek
+    Math.random()   0 - 1 (np. 0.214234)
+    Math.random() * 5     0 - 5 (np. 3.2342424)
+    Math.round() | Math.floor() | Math.ceil()
+
+*/
